b6c8cf7b79ac153475aa58784b4f5a9550295ad3
vinaxue <43913920+vinaxue@users.noreply.github.com>
2022-02-07 23:57:35 -0600

    sidebar

--- smancha/src/App.js
+++ smancha/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Route, Routes, Navigate, BrowserRouter as Router } from 'react-router-dom';
+
+import Home from './components/Home/Home'
+import ResearchInterests from './components/ResearchInterests/ResearchInterests';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Routes>
+					<Route
+						exact path='/'
+						element={<Home />}
+					/>
+					<Route
+						exact path='research-interests'
+						element={<ResearchInterests />}
+					/>
+					{/* <Route path="*">
+						<Navigate to="/" />
+					</Route> */}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
