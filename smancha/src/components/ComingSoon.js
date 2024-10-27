2b560c5bded25b2e252bf3f85490baa94403f634
vinaxue <43913920+vinaxue@users.noreply.github.com>
2024-10-27 16:41:00 -0400

    Update ComingSoon.js

--- smancha/src/components/ComingSoon.js
+++ smancha/src/components/ComingSoon.js
@@ -24,7 +24,7 @@ class ComingSoon extends Component {
 						<div className='contact-info text-left' style={{ margin: '40px' }}>
 							<div><b>Email: </b><a className='dark text-decoration-none' target='_blank' href='mailto:smancha1231@utexas.edu'>smancha1231@utexas.edu</a></div>
 							<div><b>LinkedIn: </b><a className='dark text-decoration-none' target='_blank' href='https://www.linkedin.com/in/sebastian-a-mancha-275229212'>https://www.linkedin.com/in/sebastian-a-mancha-275229212</a></div>
-							<div><b>Location:</b> Austin, TX </div>
+							<div><b>Location:</b> Washington, DC </div>
 						</div>
 					</div>
 				</div>
